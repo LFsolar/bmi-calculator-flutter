6f02f454fff12c221a42c36345db2ac749885073
Lisa Sun <sunlisa@gmail.com>
2022-02-13 00:20:20 -0600

    change to dark theme + custom

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+// main.dart has themes and app name
+
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,29 +9,25 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          primary: Color(0xFF0A0E21),
+        ),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSwatch().copyWith(
+      //     primary: Color(0xFFC3AAA5),
+      //     secondary: Color(0xFF2D303A),
+      //   ),
+      //   scaffoldBackgroundColor: Color(0xFFB8B5B8),
+      //   textTheme: TextTheme(
+      //     bodyText2: TextStyle(
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      // ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
