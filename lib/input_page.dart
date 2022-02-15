879834d7b4c98e3a89bd8290ad70f80d7c1c9a31
Lisa Sun <sunlisa@gmail.com>
2022-02-14 22:00:00 -0600

    refactor cards

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -12,24 +12,38 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Row(
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
               children: [
-                Expanded(),
-                Expanded(),
+                Expanded(
+                  child: ReusableCard(
+                    colour: Color(0xFF1D1E33),
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
               ],
             ),
-            Expanded(),
-            Row(
+          ),
+          Expanded(
+            child: ReusableCard(),
+          ),
+          Expanded(
+            child: Row(
               children: [
-                Expanded(),
-                Expanded(),
+                Expanded(
+                  child: ReusableCard(),
+                ),
+                Expanded(
+                  child: ReusableCard(),
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       // floatingActionButton: Theme(
       //   data: ThemeData(
@@ -40,6 +54,21 @@ class _InputPageState extends State<InputPage> {
       //     child: Icon(Icons.add),
       //   ),
       // ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  ReusableCard({this.colour = const Color(0xFF1D1E33)});
+  final Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10),
+      ),
     );
   }
 }
