122834bd0b1242284bd484c1b0490e8347625c29
Lisa Sun <sunlisa@gmail.com>
2022-03-09 22:13:55 -0600

    chnage slider theme

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,17 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark().copyWith(
+        // sliderTheme: SliderThemeData(
+        //   activeTrackColor: Colors.white,
+        //   thumbColor: Color(0xFFEB1555),
+        //   overlayColor: Color(0x29EB1555),
+        //   thumbShape: RoundSliderThumbShape(
+        //     enabledThumbRadius: 15,
+        //   ),
+        //   overlayShape: RoundSliderOverlayShape(
+        //     overlayRadius: 30,
+        //   ),
+        // ),
         colorScheme: ColorScheme.fromSwatch().copyWith(
           primary: Color(0xFF0A0E21),
         ),
